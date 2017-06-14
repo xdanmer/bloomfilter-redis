323a0d4006b768a2681411255bd353546a5b3ccc
Evgeny Pavlov <epavlov@news360.ru>
2017-06-14 19:54:01 +0300

    fix tests using asyncio and pytest

--- tests.py
+++ tests.py
@@ -1,80 +1,103 @@
-import random
-import redis
-import sys
-import unittest
+import asyncio
+from datetime import datetime, timedelta
 
-from datetime import datetime,timedelta
+import aioredis
+import pytest
 
 from bloomfilter import BloomFilter, TimeSeriesBloomFilter
 
-class SimpleTestCase(unittest.TestCase):
-    def setUp(self):
-        self.connection = redis.Redis()
-
-        self.single = BloomFilter(connection=self.connection,
-                        bitvector_key='test_bloomfilter',
-                        n=1024,
-                        k=4)
-
-        self.timeseries = TimeSeriesBloomFilter(connection=self.connection,
-                            bitvector_key='test_timed_bloomfilter',
-                            n=1024*8,
-                            k=4,
-                            time_resolution=timedelta(microseconds=1000),
-                            time_limit=timedelta(microseconds=10000))
-
-    def tearDown(self):
-        # remove the key in redis
-        self.connection.delete('test_bloomfilter')
-
-class SimpleTest(SimpleTestCase):
-    def test_add(self):
-        f = self.single
-
-        f.add('three')
-        f.add('four')
-        f.add('five')
-        f.add('six')
-        f.add('seven')
-        f.add('eight')
-        f.add('nine')
-        f.add("ten")
-
-        # test membership operations
-        assert 'ten' in f
-        assert 'five' in f
-        assert 'two' not in f
-        assert 'eleven' not in f
-
-    def test_delete(self):
-        f = self.single
-
-        f.add('ten')
-        assert 'ten' in f
-
-        f.delete('ten')
-        assert 'ten' not in f
+loop = asyncio.get_event_loop_policy().new_event_loop()
+asyncio.set_event_loop(loop)
 
 
-    def test_timeseries_add(self):
-        f = self.timeseries
+async def create_redis():
+    conn_pool = await aioredis.create_pool(('127.0.0.1', 6379), db=0, maxsize=5)
+    async with conn_pool.get() as conn:
+        conn.flushdb()
 
-        assert 'test_value' not in f
-        f.add('test_value')
-        assert 'test_value' in f
+    return conn_pool
 
-    def test_timeseries_delay(self):
-        f = self.timeseries
 
-        f.add('test_value')
-        start = datetime.now()
-        # allow for 3ms delay in storing/timer resolution
-        delay = timedelta(microseconds=3000)
+async def create_single(request):
+    conn_pool = await create_redis()
+    single = BloomFilter(conn_pool=conn_pool,
+                         bitvector_key='test_bloomfilter',
+                         bits_count=1024*8,
+                         hashes_count=4)
 
-        # make sure that the filter doesn't say that test_value is in the filter for too long
-        while 'test_value' in f:
-            assert datetime.now() < (start+timedelta(microseconds=10000)+delay)
-        assert 'test_value' not in f
+    request.addfinalizer(conn_pool.close)
+    return single
 
-if __name__ == '__main__':
-    unittest.main()
+
+async def create_time_series(request):
+    conn_pool = await create_redis()
+    timeseries = TimeSeriesBloomFilter(conn_pool=conn_pool,
+                                       bitvector_key='test_timed_bloomfilter',
+                                       capacity=100000,
+                                       error_rate=0.01,
+                                       time_resolution=timedelta(microseconds=1000),
+                                       time_limit=timedelta(microseconds=10000))
+
+    request.addfinalizer(conn_pool.close)
+    return timeseries
+
+
+@pytest.mark.asyncio
+async def test_add(request):
+    f = await create_single(request)
+
+    await f.add_async(['three',
+                       'four',
+                       'five',
+                       'six',
+                       'seven',
+                       'eight',
+                       'nine',
+                       'ten'])
+    # test membership operations
+    res = await f.contains_async(['ten', 'five', 'two', 'eleven'])
+
+    assert res['ten']
+    assert res['five']
+    assert not res['eleven']
+    assert not res['two']
+
+
+@pytest.mark.asyncio
+async def test_delete(request):
+    f = await create_single(request)
+
+    await f.add_async(['ten'])
+    contained = await f.contains_async(['ten', 'five', 'two', 'eleven'])
+    assert contained['ten']
+
+    await f.delete_async('ten')
+    contained = await f.contains_async(['ten', 'five', 'two', 'eleven'])
+    assert not contained['ten']
+
+
+@pytest.mark.asyncio
+async def test_timeseries_add(request):
+    f = await create_time_series(request)
+
+    contained = await f.contains_async(['test_value'])
+    assert not contained['test_value']
+
+    await f.add_async(['test_value'])
+    contained = await f.contains_async(['test_value'])
+    assert contained['test_value']
+
+
+@pytest.mark.asyncio
+async def test_timeseries_delay(request):
+    f = await create_time_series(request)
+
+    await f.add_async(['test_value'])
+    start = datetime.now()
+    # allow for 3ms delay in storing/timer resolution
+    delay = timedelta(microseconds=3000)
+
+    # make sure that the filter doesn't say that test_value is in the filter for too long
+    while (await f.contains_async(['test_value']))['test_value']:
+        assert datetime.now() < (start + timedelta(microseconds=10000) + delay)
+    assert not (f.contains_async(['test_value']))['test_value']
