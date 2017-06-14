3fac26f532eb6e682e9ccea2786a3fc81931ef10
Evgeny Pavlov <epavlov@news360.ru>
2017-06-14 18:01:30 +0300

    fix benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,54 +1,94 @@
+import asyncio
 import random
-import redis
-import sys
+from datetime import datetime, timedelta
 
-from datetime import datetime
+from redis import StrictRedis
 
-from bloomfilter import BloomFilter, TimeSeriesBloomFilter
+from bloomfilter import TimeSeriesBloomFilter
 
-connection = redis.Redis()
-
-filter_size = 8 * 500 * 1024
 test_amount = 1000 * 10
+capacity = 24000000
+batch = 500
 
-TimeSeriesBloomFilter
-f = TimeSeriesBloomFilter(connection=connection, bitvector_key='test_bloomfilter', n=filter_size, k=4)
-# f = BloomFilter(connection=connection, bitvector_key='test_bloomfilter', n=filter_size, k=4)
+host = 'localhost'
+port = 6379
+db = 0
 
-print "filling bloom filter of %.2fkB size with %.1fk values" % \
-    (filter_size/1024.0/8, test_amount/1000.0)
 
-# create a reference dict so that we can check for false positives
-ref = {}
-for x in xrange(test_amount):
-    ref['%.8f' % random.random()] = True
+async def main():
+    r = StrictRedis(host=host, port=port, db=db)
+    r.flushdb()
 
-# add values to filter
-start = datetime.now()
-for val in ref.iterkeys():
-    f.add(val)
+    bloom = TimeSeriesBloomFilter(bitvector_key='bloom',
+                                  capacity=capacity,
+                                  error_rate=0.01,
+                                  time_limit=timedelta(days=3),
+                                  time_resolution=timedelta(hours=36))
 
-# calculate results
-duration = datetime.now()-start
-duration = duration.seconds+duration.microseconds/1000000.0
-per_second = test_amount/duration
-print "adding %i values took %.2fs (%i values/sec, %.2f us/value)" % \
-    (test_amount, duration, per_second, 1000000.0/per_second)
+    await bloom.connect_async(host=host, port=port, db=db)
 
-# try random values and see how many false positives we'll get
-false_positives = 0
-correct_responses = 0
-start = datetime.now()
-while correct_responses < test_amount:
-    val = '%.8f' % random.random()
-    if (val in f) and (val not in ref):
-        false_positives += 1
-    else:
-        correct_responses += 1
-duration = datetime.now()-start
-print "duration:",duration
-print "correct: %s / false: %s -> %.4f%% false positives" % \
-    (correct_responses, false_positives, 100*false_positives/float(correct_responses))
+    print("filling bloom filter of %.2fkB size with %.1fk values" % \
+          (bloom._bits_count / 1024.0 / 8, test_amount / 1000.0))
 
-# remove the key in redis
-connection.delete('test_bloomfilter')
+    # create a reference dict so that we can check for false positives
+    ref = {}
+    for x in range(test_amount):
+        ref['%.8f' % random.random()] = True
+
+    # add values to filter
+    keys = list(ref.keys())
+
+    start = datetime.now()
+    futures = []
+    for i in range(len(keys) // batch):
+        to_add = [k for k in keys[i:i + batch]]
+        future = bloom.add_async(to_add)
+        futures.append(future)
+    await asyncio.gather(*futures)
+
+    # calculate results
+    duration = datetime.now() - start
+    duration = duration.seconds + duration.microseconds / 1000000.0
+    per_second = test_amount / duration
+    print("adding %i values took %.5fs (%i values/sec, %.2f us/value)" % \
+          (test_amount, duration, per_second, 1000000.0 / per_second))
+
+    # try random values and see how many false positives we'll get
+    false_positives = 0
+    correct_responses = 0
+    start = datetime.now()
+    while correct_responses < test_amount:
+        futures = []
+        for i in range(10):
+            vals = ['%.8f' % random.random() for i in range(batch)]
+            futures.append(bloom.contains_async(vals))
+
+        print("start gather: ", datetime.now())
+
+        results = await asyncio.gather(*futures)
+
+        duration = datetime.now() - start
+        print("gather completed. duration: ", duration)
+
+        for res in results:
+            for key, is_contained in res.items():
+                if is_contained and (key not in ref):
+                    false_positives += 1
+                else:
+                    correct_responses += 1
+
+        duration = datetime.now() - start
+        print("counting completed. duration: ", duration)
+
+    duration = datetime.now() - start
+    print("checking duration:", duration)
+    print("correct: %s / false: %s -> %.4f%% false positives" % \
+          (correct_responses, false_positives, 100 * false_positives / float(correct_responses)))
+
+    r.flushdb()
+
+
+loop = asyncio.get_event_loop_policy().new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(main())
+loop.close()
